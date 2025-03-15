44c0a6b3f039394f681df11bee7378fc30a9d96b
@Aoshi346 <aoshi3460@gmail.com>
2025-03-15 18:41:13 -0400

    testing autoplay

--- JS/background_music.js
+++ JS/background_music.js
@@ -1,63 +1,78 @@
 document.addEventListener('DOMContentLoaded', function() {
     const audio = document.getElementById('background-music');
-    const toggleButton = document.getElementById('toggle-music');
-    const musicIcon = toggleButton.querySelector('i');
+    const musicControl = document.getElementById('toggle-music');
+    const musicIcon = musicControl.querySelector('i');
     
     // Set initial volume
     audio.volume = 0.3;
 
-    // Function to toggle music
+    // Function to handle music toggle
     function toggleMusic() {
         if (audio.paused) {
-            audio.play()
-                .then(() => {
-                    musicIcon.classList.remove('fa-music');
-                    musicIcon.classList.add('fa-pause');
-                    toggleButton.classList.add('playing');
-                })
-                .catch(error => {
-                    console.error('Error playing audio:', error);
-                });
+            audio.play();
+            musicIcon.classList.remove('fa-volume-mute');
+            musicIcon.classList.add('fa-music');
+            musicControl.classList.add('playing');
         } else {
             audio.pause();
-            musicIcon.classList.remove('fa-pause');
-            musicIcon.classList.add('fa-music');
-            toggleButton.classList.remove('playing');
+            musicIcon.classList.remove('fa-music');
+            musicIcon.classList.add('fa-volume-mute');
+            musicControl.classList.remove('playing');
         }
     }
 
-    // Add click event listener to toggle button
-    toggleButton.addEventListener('click', toggleMusic);
+    // Only attach click event to the music control button
+    musicControl.addEventListener('click', function(e) {
+        e.preventDefault();
+        toggleMusic();
+    });
 
-    // Handle page visibility change
+    // Function to start playing music
+    function startMusic() {
+        audio.play()
+            .then(() => {
+                musicIcon.classList.remove('fa-volume-mute');
+                musicIcon.classList.add('fa-music');
+                musicControl.classList.add('playing');
+            })
+            .catch(error => {
+                console.log("Autoplay prevented by browser. Please click to play.");
+                // Show visual indication that music needs to be started manually
+                musicIcon.classList.remove('fa-music');
+                musicIcon.classList.add('fa-volume-mute');
+                musicControl.classList.remove('playing');
+            });
+    }
+
+    // Try to start music immediately
+    startMusic();
+
+    // Also try to start music on first user interaction with the page
+    const startMusicOnce = () => {
+        startMusic();
+        document.removeEventListener('click', startMusicOnce);
+        document.removeEventListener('touchstart', startMusicOnce);
+        document.removeEventListener('keydown', startMusicOnce);
+    };
+
+    document.addEventListener('click', startMusicOnce);
+    document.addEventListener('touchstart', startMusicOnce);
+    document.addEventListener('keydown', startMusicOnce);
+
+    // Handle visibility change to pause/resume music
     document.addEventListener('visibilitychange', function() {
         if (document.hidden) {
             audio.pause();
-            musicIcon.classList.remove('fa-pause');
-            musicIcon.classList.add('fa-music');
-            toggleButton.classList.remove('playing');
+            musicIcon.classList.remove('fa-music');
+            musicIcon.classList.add('fa-volume-mute');
+            musicControl.classList.remove('playing');
+        } else if (!audio.paused) {
+            audio.play()
+                .then(() => {
+                    musicIcon.classList.remove('fa-volume-mute');
+                    musicIcon.classList.add('fa-music');
+                    musicControl.classList.add('playing');
+                });
         }
     });
-
-    // Handle user interaction requirement for autoplay
-    function handleFirstInteraction() {
-        // Try to play audio on first interaction
-        audio.play()
-            .then(() => {
-                musicIcon.classList.remove('fa-music');
-                musicIcon.classList.add('fa-pause');
-                toggleButton.classList.add('playing');
-            })
-            .catch(error => {
-                console.error('Error playing audio:', error);
-            });
-        
-        // Remove the event listeners after first interaction
-        document.removeEventListener('click', handleFirstInteraction);
-        document.removeEventListener('touchstart', handleFirstInteraction);
-    }
-
-    // Add event listeners for first interaction
-    document.addEventListener('click', handleFirstInteraction);
-    document.addEventListener('touchstart', handleFirstInteraction);
 }); 
